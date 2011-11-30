7dd335453e2350ae918a42f82c27d238e8b945f7
marcello@10tico.com <chimp@new-host.home>
2011-11-29 22:42:16 -0500

    Updated headers and footers

--- application/views/main.php
+++ application/views/main.php
@@ -3,13 +3,13 @@
 <!-- login page -->
 <div data-role="page" id="loginview"> 
 <?php  $this->load->view('utility/fb_js_sdk'); ?>
-	<div data-role="header"> 
-		<h1>VINOZO: Login</h1>
+	<div data-role="header" data-position="fixed"> 
+		<h1>VINOZO</h1>
 		
 	</div><!-- /header --> 
 	<div data-role="content" > 
 		<div>
-		  <h1>Vinozo - Login</h1>
+		  <h1>Login</h1>
 		  
 		  <form action="/user/login"  data-ajax="false" method="get" id="loginform">
 		  	email:<input type="text" name="email"/><br />
@@ -20,16 +20,26 @@
 		  <?php 
 		 
 		 //var_dump($this->session->userdata); ?>
+		 
+	     <h1>Signup</h1>
+		  
+		  <form action="/user/signup"  data-ajax="false" method="get" id="loginform">
+		  	email:<input type="text" name="email"/><br />
+		  	password: <input type="password" name="password"/>
+		  	<input type="submit" value="login" data-role="button"/>
+		  </form>
+	
 		</div>
 	</div> 
+	<?php  $this->load->view('templates/footer'); ?> 
 </div> 
 <!-- /login page -->
 
 <!-- search page -->
 <div data-role="page" id="searchview"> 
 <?php  $this->load->view('utility/search_ajax'); ?>
-	<div data-role="header"> 
-		<h1>VINOZO: Search</h1>
+	<div data-role="header" data-position="fixed"> 
+		<h1>Search</h1>
 	</div><!-- /header --> 
 	
 	<div data-role="content" > 
@@ -40,7 +50,6 @@
 		<div id="wineresults">
 			
 		</div>
-		<p><a href="/user/logout" rel="external">logout</a></p>
 		 
 	</div>
 <!-- /search page -->
