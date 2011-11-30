804fddfdcadd68825621cd3b38635c25cac8dd78
marcello@10tico.com <chimp@new-host.home>
2011-11-29 22:24:33 -0500

    Updated with tabs

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,14 @@
 
+<div data-role="footer" data-position="fixed">
+		<div data-role="navbar">
+			<ul>
+				<li><a href="#favorites" id="tabA" data-icon="custom">Favorites</a></li>
+				<li><a href="#checkin" id="tabB" data-icon="custom">CheckIn</a></li>
+				<li><a href="#profile" id="tabC" data-icon="custom" class="ui-btn-active">Profile</a></li>
+				<li><a href="#friends" id="tabD" data-icon="custom">Friends</a></li>
+			</ul>
+		</div><!-- /navbar -->
+
 <!-- scripts concatenated and minified via ant build script-->
 <script src="/_assets/js/plugins.js"></script>
 <script src="/_assets/js/script.js"></script>
